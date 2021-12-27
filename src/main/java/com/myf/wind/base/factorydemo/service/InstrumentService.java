bc09e6d803e6b2401b36c94b42f42a2eea91604e
windmyf <windmyf701@163.com>
2021-12-28 07:58:42 +0800

    service层工厂类实现

---
+++ src/main/java/com/myf/wind/base/factorydemo/service/InstrumentService.java
@@ -0,0 +1,15 @@
+package com.myf.wind.base.factorydemo.service;
+/**
+ * @author : wind-myf
+ * @desc : 乐器类 Service
+ * @version : 1.0
+ */
+public interface InstrumentService {
+
+    /**
+     * 弹奏乐器
+     * @param userName 用户名
+     * @return String
+     */
+    String playInstrument(String userName);
+}
